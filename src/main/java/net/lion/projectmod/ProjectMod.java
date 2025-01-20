6cfe7d402ed24320480b50f262a4c85fccf4cf8e
lion0noil <poopetlion@gmail.com>
2025-01-20 17:20:03 +0700

    very important change

--- src/main/java/net/lion/projectmod/ProjectMod.java
+++ src/main/java/net/lion/projectmod/ProjectMod.java
@@ -14,7 +14,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-//very important
+//very important note
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(ProjectMod.MOD_ID)
 public class ProjectMod {
