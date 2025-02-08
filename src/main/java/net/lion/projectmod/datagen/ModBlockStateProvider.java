6cf6df4f66b479057550bc18068cc5a6e611b9bc
lion0noil <poopetlion@gmail.com>
2025-02-08 16:59:41 +0700

    update

---
+++ src/main/java/net/lion/projectmod/datagen/ModBlockStateProvider.java
@@ -0,0 +1,30 @@
+package net.lion.projectmod.datagen;
+
+import net.lion.projectmod.ProjectMod;
+import net.lion.projectmod.block.ModBlocks;
+import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
+        super(output, ProjectMod.MOD_ID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        blockWithItem(ModBlocks.ALEXANDRITE_BLOCK);
+        blockWithItem(ModBlocks.RAW_ALEXANDRITE_BLOCK);
+
+        blockWithItem(ModBlocks.ALEXANDRITE_ORE);
+        blockWithItem(ModBlocks.ALEXANDRITE_DEEPSLATE_ORE);
+
+        blockWithItem(ModBlocks.MAGIC_BLOCK);
+    }
+
+    private void blockWithItem(RegistryObject<Block> blockRegistryObject) {
+        simpleBlockWithItem(blockRegistryObject.get(), cubeAll(blockRegistryObject.get()));
+    }
+}
